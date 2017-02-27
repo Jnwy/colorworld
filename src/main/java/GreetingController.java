05d415fe892cf0d0d404b7c73db2a3ac06856036
Jhang Junwei <j.junwei@gmail.com>
2017-02-27 14:39:43 +0800

    基本結構

---
+++ src/main/java/GreetingController.java
@@ -0,0 +1,27 @@
+package java;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping("/greetingService")
+	public Greeting greetingService(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+	@RequestMapping("/greeting")
+	public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+}
